e5c35bd4482bf901ec61fc9762a316caa33d19ca
heemang2001 <babypooh2001@naver.com>
2021-10-26 21:03:58 +0900

    단체사진 찍기

---
+++ Programmers/단체사진 찍기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+int solution(int n, vector<string> data)
+{
+	int answer = 0;
+
+
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution(2, { "N~F=0", "R~T>2" }) << '\n'; // 3648
+	//cout << solution(2, { "M~C<2", "C~M>1" }) << '\n'; // 0
+
+	return 0;
+}
